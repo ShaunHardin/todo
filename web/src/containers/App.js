d3259e716f176f5eaf1c45504b81687ae4ef3ec5
Shaun Hardin <shardin@covermymeds.com>
2016-11-29 21:53:55 -0500

    Remove router and form (at least until I need them)
    - Rename things to 'TodoList'
    - Switch TodoList component to class style

--- web/src/containers/App.js
+++ web/src/containers/App.js
@@ -1,18 +1,13 @@
 // @flow
 import React, { Component } from 'react';
-import { BrowserRouter, Match, Miss } from 'react-router'
-import Home from './Home';
-import NotFound from './NotFound';
+import TodoList from './TodoList';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Match exactly pattern="/" component={Home} />
-          <Miss component={NotFound} />
-        </div>
-      </BrowserRouter>
+      <div>
+        <TodoList />
+      </div>
     );
   }
 }
---
+++ web/src/containers/TodoList/index.js
@@ -0,0 +1,15 @@
+// @flow
+import React from 'react';
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Todos</h2>
+        <div>Hello world!</div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
